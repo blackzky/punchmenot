3e8da4548228549361bad59458f1f22147fe6d87
Gervel Giva <gervel@caresharing.ph>
2012-08-31 23:46:55 +0800

    working movement+scoket.io

--- scripts/script.js
+++ scripts/script.js
@@ -4,113 +4,118 @@ var CLIENT_SIDE_PLAYER_LIST;
 var WORLD_MAP_DATA;
 var CURRENT_PLAYER;
 
-socket.on('connect', function () {
-	var player_name = prompt("Enter name:");
-	socket.emit('addUser', player_name);
-	
-	socket.on('initPlayerConfig', function(world_map, player){
-		WORLD_MAP_DATA = world_map;
-		CURRENT_PLAYER = player;
-		renderPlayerMap();
-	});
-	
-	socket.on('setPlayerList', function(players) {
-		CLIENT_SIDE_PLAYER_LIST = players;
-		$("div#player_list ul").empty();
-		for(id in players){
-			$("div#player_list ul").append("<li>" + players[id].name + "</li>");
-		} 
-		updatePlayerMap();
-		
-	});
-});
 
 function getTile(row, col){
-	return $("#" + row + "_" + col);
+  return $("#" + row + "_" + col);
 }
 function renderPlayerMap(){
-	var html = "";
-	for(row in WORLD_MAP_DATA){
-		html += "<div class='row'>";
-		for(col in WORLD_MAP_DATA[row]){
-			html += "<div class='column'><div id=" + row + "_" + col + " class='" + WORLD_MAP_DATA[row][col] + "'></div></div>";
-		}
-		html += "</div>";
-	}
-	$("#game_canvas").html(html);
+  var html = "";
+  for(row in WORLD_MAP_DATA){
+    html += "<div class='row'>";
+    for(col in WORLD_MAP_DATA[row]){
+      html += "<div class='column'><div id=" + row + "_" + col + " class='" + WORLD_MAP_DATA[row][col] + "'></div></div>";
+    }
+    html += "</div>";
+  }
+  $("#game_canvas").html(html);
 }
 function updatePlayerMap(){
-	var tile;
-	for(id in CLIENT_SIDE_PLAYER_LIST){
-		tile = getTile(CLIENT_SIDE_PLAYER_LIST[id].row, CLIENT_SIDE_PLAYER_LIST[id].col);
-		tile.attr("class", "player");
-	}
+  var tile;
+  for(id in CLIENT_SIDE_PLAYER_LIST){
+    tile = getTile(CLIENT_SIDE_PLAYER_LIST[id].row, CLIENT_SIDE_PLAYER_LIST[id].col);
+    tile.attr("class", "player");
+  }
 }
 
-
 $(document).ready(function() {
-	$(document).keydown(function(event) {
-		socket.emit('keyDownInput', event.keyCode);
-	});
+  socket.on('connect', function () {
+    var player_name = prompt("Enter name:");
+    socket.emit('addUser', player_name);
+
+    socket.on('initPlayerConfig', function(world_map, player){
+      WORLD_MAP_DATA = world_map;
+      CURRENT_PLAYER = player;
+      renderPlayerMap();
+    });
+
+    socket.on('setPlayerList', function(players) {
+      CLIENT_SIDE_PLAYER_LIST = players;
+      $("div#player_list ul").empty();
+      for(id in players){
+        $("div#player_list ul").append("<li>" + players[id].username + "</li>");
+      } 
+      updatePlayerMap();
+    });
+
+    socket.on('updateWorldMap', function(map){
+      WORLD_MAP_DATA = map;
+      renderPlayerMap(); 
+    });
+  });
+
+
+  $(document).keydown(function(event) {
+      socket.emit('keyDownInput', event.keyCode);
+  });
 
 });
 
 
 
 /*
-function movePlayer(event){
-	var current_tile, adjacent_tile;
-	current_tile = getTile($PLAYER.row, $PLAYER.col);
-	switch(event.keyCode){
-			case $KEY.w:
-				adjacent_tile = getPlayerAdjacentTile("up");
-				if(adjacent_tile.attr("class") == "empty"){
-					current_tile.attr("class", "empty");
-					$PLAYER.row--;
-					adjacent_tile.attr("class", "player");
-				}
-			break;
-			case $KEY.a:
-				adjacent_tile = getPlayerAdjacentTile("left");
-				if(adjacent_tile.attr("class") == "empty"){
-					current_tile.attr("class", "empty");
-					$PLAYER.col--;
-					adjacent_tile.attr("class", "player");
-				}
-			break;
-			case $KEY.s:
-				adjacent_tile = getPlayerAdjacentTile("down");
-				if(adjacent_tile.attr("class") == "empty"){
-					current_tile.attr("class", "empty");
-					$PLAYER.row++;
-					adjacent_tile.attr("class", "player");
-				}
-			break;
-			case $KEY.d:
-				adjacent_tile = getPlayerAdjacentTile("right");
-				if(adjacent_tile.attr("class") == "empty"){
-					current_tile.attr("class", "empty");
-					$PLAYER.col++;
-					adjacent_tile.attr("class", "player");
-				}
-			break;
-		}
-}
+   function movePlayer(event){
+   var current_tile, adjacent_tile;
+   current_tile = getTile($PLAYER.row, $PLAYER.col);
+   switch(event.keyCode){
+   case $KEY.w:
+   adjacent_tile = getPlayerAdjacentTile("up");
+   if(adjacent_tile.attr("class") == "empty"){
+   current_tile.attr("class", "empty");
+   $PLAYER.row--;
+   adjacent_tile.attr("class", "player");
+   }
+   break;
+   case $KEY.a:
+   adjacent_tile = getPlayerAdjacentTile("left");
+   if(adjacent_tile.attr("class") == "empty"){
+   current_tile.attr("class", "empty");
+   $PLAYER.col--;
+   adjacent_tile.attr("class", "player");
+   }
+   break;
+   case $KEY.s:
+   adjacent_tile = getPlayerAdjacentTile("down");
+   if(adjacent_tile.attr("class") == "empty"){
+   current_tile.attr("class", "empty");
+   $PLAYER.row++;
+   adjacent_tile.attr("class", "player");
+   }
+   break;
+   case $KEY.d:
+   adjacent_tile = getPlayerAdjacentTile("right");
+   if(adjacent_tile.attr("class") == "empty"){
+   current_tile.attr("class", "empty");
+   $PLAYER.col++;
+   adjacent_tile.attr("class", "player");
+   }
+   break;
+   }
+   }
 
-function getPlayerAdjacentTile(direction){
-	switch(direction){
-		case "up":
-			return getTile($PLAYER.row - 1, $PLAYER.col);
-		break;
-		case "down":
-			return getTile($PLAYER.row + 1, $PLAYER.col);
-		break;
-		case "left":
-			return getTile($PLAYER.row, $PLAYER.col - 1);
-		break;
-		case "right":
-			return getTile($PLAYER.row, $PLAYER.col + 1);
-		break;
-	}
-}
-*/
+   function getPlayerAdjacentTile(direction){
+   switch(direction){
+   case "up":
+   return getTile($PLAYER.row - 1, $PLAYER.col);
+   break;
+   case "down":
+   return getTile($PLAYER.row + 1, $PLAYER.col);
+   break;
+   case "left":
+   return getTile($PLAYER.row, $PLAYER.col - 1);
+   break;
+   case "right":
+   return getTile($PLAYER.row, $PLAYER.col + 1);
+   break;
+   }
+   }
+   */
--- scripts/server.js
+++ scripts/server.js
@@ -23,9 +23,9 @@ function handler(request, response) {
     case '.css':
       contentType = 'text/css';
     break;
-	case '.png':
-		contentType = 'image/png';
-	break;
+    case '.png':
+      contentType = 'image/png';
+    break;
   }
 
   path.exists(filePath, function(exists) {
@@ -52,157 +52,143 @@ function handler(request, response) {
 
 
 var WORLD_MAP_INFO = {
-	TILE_SIZE : 32,
-	MAX_COL : 25,
-	MAX_ROW : 18,
+  TILE_SIZE : 32,
+  MAX_COL : 25,
+  MAX_ROW : 18
 };
 var WORLD_MAP_DATA;
 var PLAYERS = [];
 var KEYCODE = {
-	"w" : 87,
-	"a" : 65,
-	"s" : 83,
-	"d" : 68
+  "UP" : 87,
+  "LEFT" : 65,
+  "DOWN" : 83,
+  "RIGHT" : 68
 };
 
-function initWorldMap(){
-	WORLD_MAP_DATA = Array(WORLD_MAP_INFO.MAX_ROW);
-	for(row = 0; row < WORLD_MAP_INFO.MAX_ROW; row++){
-		WORLD_MAP_DATA[row] = Array(WORLD_MAP_INFO.MAX_COL);
-		for(col = 0; col < WORLD_MAP_INFO.MAX_COL; col++){
-			WORLD_MAP_DATA[row][col] = "empty";
-		}
-	}
-}
 function generateRandomPosition(){
-	var position = {row:0, col:0};
-	position.row = parseInt(Math.random() * WORLD_MAP_INFO.MAX_ROW);
-	position.col = parseInt(Math.random() * WORLD_MAP_INFO.MAX_COL);
-	return position;
+  var position = {row:0, col:0};
+  position.row = parseInt(Math.random() * WORLD_MAP_INFO.MAX_ROW);
+  position.col = parseInt(Math.random() * WORLD_MAP_INFO.MAX_COL);
+  return position;
+}
+function initWorldMap(){
+  WORLD_MAP_DATA = Array(WORLD_MAP_INFO.MAX_ROW);
+  for(row = 0; row < WORLD_MAP_INFO.MAX_ROW; row++){
+    WORLD_MAP_DATA[row] = Array(WORLD_MAP_INFO.MAX_COL);
+    for(col = 0; col < WORLD_MAP_INFO.MAX_COL; col++){
+      WORLD_MAP_DATA[row][col] = "empty";
+    }
+  }
+  generateRandomObjects("rock", 5);
 }
 function generateRandomObjects(object, occurance){
-	if(occurance == 0){	return;	}
-	var position = generateRandomPosition();
-	var tile = WORLD_MAP_DATA[position.row][position.col];
-	if(tile == "empty"){
-		tile = object;
-		occurance--;
-		generateRandomObjects(object, occurance);
-	}else{
-		generateRandomObjects(object, occurance);
-	}
+  if(occurance == 0){	return;	}
+  var position = generateRandomPosition();
+  var tile = WORLD_MAP_DATA[position.row][position.col];
+  if(tile == "empty"){
+    WORLD_MAP_DATA[position.row][position.col] = object;
+    occurance--;
+    generateRandomObjects(object, occurance);
+  }else{
+    generateRandomObjects(object, occurance);
+  }
 }
 function initPlayer(username){
-	var position = generateRandomPosition();	
-	var tile = WORLD_MAP_DATA[position.row][position.col];
-	
-	if(tile == "empty"){
-		PLAYERS.push({
-			username : username,
-			row : position.row,
-			col : position.col
-		});
-	}else{
-		initPlayer(username);
-	}
-}
+  var position = generateRandomPosition();	
+  var tile = WORLD_MAP_DATA[position.row][position.col];
 
+  if(tile == "empty"){
+    PLAYERS.push({
+      username : username,
+      row : position.row,
+      col : position.col
+    });
+  }else{
+    initPlayer(username);
+  }
+}
 function getTile(row, col){
-	return WORLD_MAP_DATA[row][col];
+  row = parseInt(row);
+  col = parseInt(col);
+  return WORLD_MAP_DATA[row][col];
 }
-function movePlayer(event){
-	var current_tile, adjacent_tile;
-	current_tile = getTile(WORLD_MAP_DATA.row, WORLD_MAP_DATA.col);
-	switch(event.keyCode){
-			case $KEY.w:
-				adjacent_tile = getPlayerAdjacentTile("up");
-				if(adjacent_tile.attr("class") == "empty"){
-					current_tile.attr("class", "empty");
-					WORLD_MAP_DATA.row--;
-					adjacent_tile.attr("class", "player");
-				}
-			break;
-			case $KEY.a:
-				adjacent_tile = getPlayerAdjacentTile("left");
-				if(adjacent_tile.attr("class") == "empty"){
-					current_tile.attr("class", "empty");
-					WORLD_MAP_DATA.col--;
-					adjacent_tile.attr("class", "player");
-				}
-			break;
-			case $KEY.s:
-				adjacent_tile = getPlayerAdjacentTile("down");
-				if(adjacent_tile.attr("class") == "empty"){
-					current_tile.attr("class", "empty");
-					WORLD_MAP_DATA.row++;
-					adjacent_tile.attr("class", "player");
-				}
-			break;
-			case $KEY.d:
-				adjacent_tile = getPlayerAdjacentTile("right");
-				if(adjacent_tile.attr("class") == "empty"){
-					current_tile.attr("class", "empty");
-					WORLD_MAP_DATA.col++;
-					adjacent_tile.attr("class", "player");
-				}
-			break;
-		}
+function getPlayerAdjacentTile(direction, row, col){
+  switch(direction){
+    case KEYCODE.UP:
+      return getTile(row - 1, col);
+    break;
+    case KEYCODE.DOWN:
+      return getTile(row + 1, col);
+    break;
+    case KEYCODE.LEFT:
+      return getTile(row, col - 1);
+    break;
+    case KEYCODE.RIGHT:
+      return getTile(row, col + 1);
+    break;
+  }
 }
-
-function getPlayerAdjacentTile(direction){
-	switch(direction){
-		case "up":
-			return getTile(WORLD_MAP_DATA.row - 1, WORLD_MAP_DATA.col);
-		break;
-		case "down":
-			return getTile(WORLD_MAP_DATA.row + 1, WORLD_MAP_DATA.col);
-		break;
-		case "left":
-			return getTile(WORLD_MAP_DATA.row, WORLD_MAP_DATA.col - 1);
-		break;
-		case "right":
-			return getTile(WORLD_MAP_DATA.row, WORLD_MAP_DATA.col + 1);
-		break;
-	}
-}
-
 initWorldMap();
-generateRandomObjects("rock", 5);
+function setTile(object, row, col) {
+  WORLD_MAP_DATA[row][col] = object;
+}
+function isEmptyTile(tile){
+  return (tile == "empty");
+}
 
 io.sockets.on('connection', function (socket) {
   //socket.on('message', function () { });
 
   socket.on('addUser', function(username){
-	initPlayer(username);
-	socket.player_id = PLAYERS.length - 1;
-	console.log(socket.player_id);
-	socket.username = PLAYERS[socket.player_id].name;
-	socket.row = PLAYERS[socket.player_id].row;
-	socket.col = PLAYERS[socket.player_id].col;
-    //socket.emit('initPlayerConfig', WORLD_MAP_DATA, PLAYERS[PLAYERS.length-1]);
-    //io.sockets.emit('setPlayerList', PLAYERS);
+    initPlayer(username);
+    socket.player_id = PLAYERS.length - 1;
+    socket.username = PLAYERS[socket.player_id].name;
+    socket.row = PLAYERS[socket.player_id].row;
+    socket.col = PLAYERS[socket.player_id].col;
+    socket.emit('initPlayerConfig', WORLD_MAP_DATA, PLAYERS[socket.player_id]);
+    io.sockets.emit('setPlayerList', PLAYERS);
   });
-  
+
   socket.on('keyDownInput', function(keyCode){
-	
-	switch(keyCode){
-			case KEYCODE.w:
-				getPlayerAdjacentTile("up");
-			break;
-			case KEYCODE.a:
-				getPlayerAdjacentTile("left");
-			break;
-			case KEYCODE.s:
-				getPlayerAdjacentTile("down");
-			break;
-			case KEYCODE.d:
-				getPlayerAdjacentTile("right");
-			break;
-		}
+    var tile =  getPlayerAdjacentTile(keyCode, socket.row, socket.col);
+    if(isEmptyTile(tile)){
+      movePlayer(keyCode, socket.row, socket.col);
+    }
   });
+
+  function movePlayer(dir, row, col){
+    switch(dir){
+      case KEYCODE.UP:
+        setTile("empty", row, col);
+        socket.row = row - 1;
+        socket.col = col;
+        setTile("player", socket.row, socket.col);
+        io.sockets.emit('updateWorldMap', WORLD_MAP_DATA);
+      break;
+      case KEYCODE.DOWN:
+        setTile("empty", row, col);
+        socket.row = row + 1;
+        socket.col = col;
+        setTile("player", socket.row, socket.col);
+        io.sockets.emit('updateWorldMap', WORLD_MAP_DATA);
+      break;
+      case KEYCODE.LEFT:
+        setTile("empty", row, col);
+        socket.row = row;
+        socket.col = col - 1;
+        setTile("player", socket.row, socket.col);
+        io.sockets.emit('updateWorldMap', WORLD_MAP_DATA);
+      break;
+      case KEYCODE.RIGHT:
+        setTile("empty", row, col);
+        socket.row = row;
+        socket.col = col + 1;
+        setTile("player", socket.row, socket.col);
+        io.sockets.emit('updateWorldMap', WORLD_MAP_DATA);
+      break;
+    }
+  }
 
   socket.on('disconnect', function () { });
 
 });
-
-
